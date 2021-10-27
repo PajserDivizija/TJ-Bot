c9c09563daf5dfc07eda30249f1d3819cb47d199
java-coding-prodigy <java-coding-prodigy@github.com>
2021-10-27 18:07:17 +0530

    Some changes
    tested a failing case which failed successfully and wrote the pixel by pixel check to compare the images.

--- application/src/main/java/org/togetherjava/tjbot/commands/utils/WolfCommandUtils.java
+++ application/src/main/java/org/togetherjava/tjbot/commands/utils/WolfCommandUtils.java
@@ -1,15 +1,12 @@
 package org.togetherjava.tjbot.commands.utils;
 
-import javax.imageio.ImageIO;
 import java.awt.*;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.util.List;
 import java.awt.image.BufferedImage;
+import java.util.List;
+import java.util.stream.IntStream;
 
 public class WolfCommandUtils {
-    public static BufferedImage combineImages(List<BufferedImage> images, int width, int height)
-            throws IOException {
+    public static BufferedImage combineImages(List<BufferedImage> images, int width, int height) {
         BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_4BYTE_ABGR);
         Graphics imgGraphics = image.getGraphics();
         int resultHeight = 0;
@@ -20,4 +17,13 @@ public class WolfCommandUtils {
         return image;
     }
 
+    public static boolean compareImages(BufferedImage img1, BufferedImage img2) {
+        int width1 = img1.getWidth();
+        int height1 = img1.getHeight();
+        return width1 == img2.getWidth() && height1 == img2.getHeight()
+                && IntStream.range(0, width1)
+                    .mapToObj(x -> IntStream.range(0, height1)
+                        .anyMatch(y -> img1.getRGB(x, y) != img2.getRGB(x, y)))
+                    .noneMatch(x -> x);
+    }
 }
