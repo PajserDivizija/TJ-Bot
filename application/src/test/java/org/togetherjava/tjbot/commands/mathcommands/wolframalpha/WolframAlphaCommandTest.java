806cb5b8d2d099a216b7a355ca03e5b3232fc55f
java-coding-prodigy <java-coding-prodigy@github.com>
2021-10-28 09:34:31 +0530

    Another test

--- application/src/test/java/org/togetherjava/tjbot/commands/mathcommands/wolframalpha/WolframAlphaCommandTest.java
+++ application/src/test/java/org/togetherjava/tjbot/commands/mathcommands/wolframalpha/WolframAlphaCommandTest.java
@@ -3,21 +3,37 @@ package org.togetherjava.tjbot.commands.mathcommands.wolframalpha;
 import org.junit.jupiter.api.Test;
 import org.togetherjava.tjbot.commands.utils.WolfCommandUtils;
 
+import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.IOException;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class WolframAlphaCommandTest {
+    @Test
+    void compareImagesTest() {
+        BufferedImage image1 = new BufferedImage(100, 100, 6);
+        BufferedImage image2 = new BufferedImage(100, 100, 6);
+        image1.getGraphics().setColor(Color.RED);
+        image2.getGraphics().setColor(Color.YELLOW);
+        assertFalse(WolfCommandUtils.compareImages(image1, image2));
+    }
 
     @Test
     void combineImagesTest() {
-        BufferedImage image1 = new BufferedImage(100, 150, 6);
+        BufferedImage image1 = new BufferedImage(100, 100, 6);
         BufferedImage image2 = new BufferedImage(100, 100, 6);
         BufferedImage mergedImage = new BufferedImage(100, 200, 6);
         assertTrue(WolfCommandUtils.compareImages(mergedImage,
-                WolfCommandUtils.combineImages(List.of(image1, image2), 100, 250)));
+                WolfCommandUtils.combineImages(List.of(image1, image2), 100, 200)));
+        Graphics g1 = image1.getGraphics();
+        g1.setColor(Color.RED);
+        g1.fillRect(0, 0, 100, 100);
+        Graphics g2 = image2.getGraphics();
+        g2.setColor(Color.RED);
+        g2.fillRect(0, 0, 100, 100);
+        assertTrue(WolfCommandUtils.compareImages(mergedImage,
+                WolfCommandUtils.combineImages(List.of(image1, image2), 100, 200)));
     }
-
 }
