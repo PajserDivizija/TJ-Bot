ef9b886049604d8675aedf0fe007a36e4c474665
java-coding-prodigy <java-coding-prodigy@github.com>
2021-11-12 19:40:22 +0530

    POJO fix1

--- application/src/main/java/org/togetherjava/tjbot/commands/mathcommands/wolframalpha/DidYouMean.java
+++ application/src/main/java/org/togetherjava/tjbot/commands/mathcommands/wolframalpha/DidYouMean.java
@@ -2,11 +2,13 @@ package org.togetherjava.tjbot.commands.mathcommands.wolframalpha;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonRootName;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlText;
 
 @JsonRootName("didyoumean")
 @JsonIgnoreProperties(ignoreUnknown = true)
 final class DidYouMean {
 
+    @JacksonXmlText
     private String message;
 
     public String getMessage() {
--- application/src/main/java/org/togetherjava/tjbot/commands/mathcommands/wolframalpha/Tip.java
+++ application/src/main/java/org/togetherjava/tjbot/commands/mathcommands/wolframalpha/Tip.java
@@ -13,7 +13,7 @@ final class Tip {
     public String getText() {
         return text;
     }
-    
+
     public void setText(String text) {
         this.text = text;
     }
